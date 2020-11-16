fe0bc18faa2024d817401fb20f908335d7efb5ea
vkamalkishore007 <74551567+vkamalkishore007@users.noreply.github.com>
2020-11-16 20:23:00 +0530

    Day1 Assignment Submitted

---
+++ Assignment 1.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print('hello')
+
+A. RANIBOW
+
+# In[7]:
+
+
+name = "lets upgrade"
+name.upper()
+
+
+# In[11]:
+
+
+E = int(input("Enter the amount on Euros:"))
+print("The Amount in INR is",E*80)
+
+
+# In[12]:
+
+
+cost_price=int(input())
+selling_price=int(input())
+if(cost_price>selling_price):
+    print("Loss")
+elif(cost_price<selling_price):
+    print("Profit")
+else:
+    print("Neither")
+
+
+# In[ ]:
+
+
+
+
